9c0267658a11821373bfadee7f2c1efee8e69a93
Ashakuur1 <engshakrayare114@gmail.com>
2025-03-21 22:37:28 +0300

    Add NotFound component for handling 404 errors and user navigation

---
+++ ui/src/components/NotFound.tsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+  return (
+    <div className="flex flex-col justify-center mt-52 text-center">
+      <h1 className="text-red-600">404 | Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+      <p>
+        Go back to the{" "}
+        <Link to="/" className="underline">
+          homepage
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+export default NotFound;
