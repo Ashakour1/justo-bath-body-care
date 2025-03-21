e22433eb68873dfe3dbc9f6fbe5c2234dc2d860c
Ashakuur1 <engshakrayare114@gmail.com>
2025-03-21 22:41:54 +0300

    Add ProductType definition to structure product data

---
+++ ui/src/types/product.t.ts
@@ -0,0 +1,12 @@
+export type ProductType = {
+  id: string;
+  name: string;
+  description: string;
+  category: string;
+  price: number;
+  rating: number;
+  size: string;
+  isNew: boolean;
+  inStock: boolean;
+  image: string;
+};
