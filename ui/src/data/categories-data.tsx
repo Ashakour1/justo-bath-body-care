7470148ab45a63ebd49ff460f8c4927de20c6f6e
Ashakuur1 <engshakrayare114@gmail.com>
2025-03-21 22:40:16 +0300

    Add categories data file with predefined category entries and images

---
+++ ui/src/data/categories-data.tsx
@@ -0,0 +1,20 @@
+import { Category } from "@/types/category.t";
+
+export const Categories: Category[] = [
+  {
+    name: "Perfumes",
+    image: "/product5.jpg",
+  },
+  {
+    name: "Justo Cosmetic",
+    image: "/product2.jpg",
+  },
+  {
+    name: "Rituals",
+    image: "/product4.jpg",
+  },
+  {
+    name: "Rituals",
+    image: "/product4.jpg",
+  },
+];
