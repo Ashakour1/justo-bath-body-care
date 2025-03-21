edd62cafba9a04834f5fc6576cf6bffd1a88052a
Ashakuur1 <engshakrayare114@gmail.com>
2025-03-21 22:41:24 +0300

    Add DesignPrinciples component to outline brand values and sustainability efforts

---
+++ ui/src/pages/About/DesignPrinciples.tsx
@@ -0,0 +1,129 @@
+const DesignPrinciples = () => {
+  return (
+    <main className="mx-auto max-w-[1180px] h-full px-4 py-2 sm:px-6 sm:py-4 lg:px-8">
+      <h1 className="text-xs font-semibold  text-gray-900 sm:text-lg pt-8">
+        DESIGN PRINCIPLES
+      </h1>
+      <div className="flex flex-col gap-5">
+        <div className=" pt-5">
+          <h2 className="text-gray-700 text-base  font-medium">
+            Redefining Activewear
+          </h2>
+          <img
+            src="/banner1.jpg"
+            alt=""
+            className="w-full h-[400px] object-cover py-3"
+          />
+          <div>
+            <p className="text-base text-gray-500">
+              Aslos was founded to inspire and empower individuals to live
+              authentically and embrace their unique journeys. In today’s
+              fast-paced world, a new wave of individuals values authenticity,
+              innovation, and purpose in every aspect of life. Our activewear
+              embodies this ethos by seamlessly integrating performance,
+              versatility, and sustainability into each design.
+            </p>
+            <p className="text-base text-gray-500 py-2">
+              What sets us apart is our personal approach to product
+              development. Our designers are also users, constantly seeking what
+              is missing from their own activewear collections and adjusting
+              based on real-life experiences. This hands-on approach ensures
+              that our products are not only stylish but also highly functional
+              and comfortable for any activity
+            </p>
+          </div>
+        </div>
+        <div className="pt-5">
+          <h2 className="text-gray-700 text-base  font-medium">
+            The Aslos Standard
+          </h2>
+          <img
+            src="/banner3.png"
+            alt=""
+            className="w-full h-[400px] object-cover py-3"
+          />
+          <div>
+            <p className="text-base text-gray-500">
+              We believe that truly great design should be felt, not just seen.
+              From moisture-wicking fabrics to eco-friendly materials, our
+              designs incorporate advanced technologies that work seamlessly in
+              the background. You might not notice these features immediately,
+              but they become indispensable once you experience their benefits.
+              This philosophy is the base of the Aslos Standard.
+            </p>
+          </div>
+        </div>
+        <div className="pt-5">
+          <h2 className="text-gray-700 text-base  font-medium">Performance </h2>
+          <img
+            src="/banner1.jpg"
+            alt=""
+            className="w-full h-[400px] object-cover py-3"
+          />
+          <div>
+            <p className="text-base text-gray-500">
+              Our products are designed to meet the demands of high-performing
+              individuals. We develop technical fabrics that provide superior
+              comfort, support, and durability. Each piece is crafted to enhance
+              your performance, whether you’re hitting the gym, running a
+              marathon, or practicing yoga.
+            </p>
+          </div>
+        </div>
+        <div className="pt-5">
+          <h2 className="text-gray-700 text-base  font-medium">Versatility </h2>
+          <img
+            src="/banner1.jpg"
+            alt=""
+            className="w-full h-[400px] object-cover py-3"
+          />
+          <div>
+            <p className="text-base text-gray-500">
+              Versatility is at the core of our design philosophy. We believe
+              that your activewear should adapt to various activities and
+              environments. Our apparel transitions effortlessly from a workout
+              session to casual wear, combining functionality with modern
+              aesthetics. We strive to create pieces that are not only practical
+              but also stylish enough to wear throughout your day.
+            </p>
+          </div>
+        </div>
+        <div className="pt-5 pb-10">
+          <h2 className="text-gray-700 text-base  font-medium">
+            Sustainability
+          </h2>
+          <img
+            src="/banner1.jpg"
+            alt=""
+            className="w-full h-[400px] object-cover py-3"
+          />
+          <div className="flex flex-col gap-2">
+            <p className="text-base text-gray-500">
+              Sustainability is a key component of the Aslos Standard. We are
+              committed to using eco-friendly materials and ethical production
+              methods. Our goal is to minimize our environmental impact while
+              delivering high-quality products. We continuously seek innovative
+              ways to reduce waste and promote sustainable practices in every
+              aspect of our business.
+            </p>
+            <p className="text-base text-gray-500">
+              We are driven by a passion for solving problems through design and
+              integrating these solutions seamlessly into our products. Every
+              Aslos item offers a unique advantage over similar products from
+              other brands. We are not just creating activewear; we are
+              perfecting it.
+            </p>
+            <p className="text-base text-gray-500">
+              Whether our designs encourage you to push your limits, explore new
+              activities, or simply feel more confident in your daily life, we
+              are dedicated to creating the future of activewear. How you use it
+              is entirely up to you.
+            </p>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default DesignPrinciples;
