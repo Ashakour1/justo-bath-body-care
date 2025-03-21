e5d0dbbc7c798a1121f705bcb68cf90d0e6c5684
Ashakuur1 <engshakrayare114@gmail.com>
2025-03-21 22:40:24 +0300

    Add products data file with sample product entries and images

---
+++ ui/src/data/products-data.tsx
@@ -0,0 +1,55 @@
+// import { ProductType } from "../types/product.t";
+// export const products: ProductType[] = [
+//   {
+//     name: "Nike Air",
+//     description:
+//       "The Nike Air is a sneaker that needs no introduction. It's been a staple in the sneaker community for years and has been worn by everyone from athletes to celebrities.",
+//     sex: "Male",
+//     category: "Run",
+//     price: 100,
+//     stock: 10,
+//     size: ["S", "M", "L", "XL"],
+//     color: ["Black", "White", "Blue"],
+//     image:
+//       "https://asrv.com/cdn/shop/files/BLACK_ASRV_EstablishedTee_c42f7fce-0293-4b1a-993d-c180b9ea2131_600x.jpg?v=1710701915",
+//   },
+//   {
+//     name: "Adidas Boost",
+//     description:
+//       "The Adidas Boost is a sneaker that needs no introduction. It's been a staple in the sneaker community for years and has been worn by everyone from athletes to celebrities.",
+//     sex: "Female",
+//     category: "Recap",
+//     price: 120,
+//     stock: 15,
+//     size: ["S", "M", "L", "XL"],
+//     color: ["Black", "White", "Blue"],
+//     image:
+//       "https://asrv.com/cdn/shop/products/ASRVE-Comm-425-Edit_600x.jpg?v=1673555835",
+//   },
+//   {
+//     name: "New Balance",
+//     description:
+//       "The New Balance is a sneaker that needs no introduction. It's been a staple in the sneaker community for years and has been worn by everyone from athletes to celebrities.",
+//     sex: "Female",
+//     category: "Train",
+//     price: 80,
+//     stock: 5,
+//     size: ["S", "M", "L", "XL"],
+//     color: ["Black", "White", "Blue"],
+//     image:
+//       "https://img.lazcdn.com/g/p/8c4ba43f51e014b168ef78746994ce79.jpg_960x960q80.jpg_.webp",
+//   },
+//   {
+//     name: "Puma",
+//     description:
+//       "The Puma is a sneaker that needs no introduction. It's been a staple in the sneaker community for years and has been worn by everyone from athletes to celebrities.",
+//     sex: "Male",
+//     category: "Run",
+//     price: 90,
+//     stock: 20,
+//     size: ["S", "M", "L", "XL"],
+//     color: ["Black", "White", "Blue"],
+//     image:
+//       "https://asrv.com/cdn/shop/products/ASRVE-Comm-425-Edit_600x.jpg?v=1673555835",
+//   },
+// ];
