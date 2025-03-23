0e0cf7d7af0ab29cb63f9d296c43656d1aad91f3
Ashakuur1 <engshakrayare114@gmail.com>
2025-03-23 11:44:00 +0300

    Configure CORS to allow requests from any origin

--- server/server.js
+++ server/server.js
@@ -10,7 +10,11 @@ const app = express();
 
 dotenv.config();
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: '*'
+  }
+));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
