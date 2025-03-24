7538634634e237dc8b243d18a2d2336539d87115
Ashakuur1 <engshakrayare114@gmail.com>
2025-03-24 08:55:18 +0300

    Increase the number of product columns in the Products section layout

--- ui/src/components/Products/products-section.tsx
+++ ui/src/components/Products/products-section.tsx
@@ -47,7 +47,7 @@ const Products = () => {
               </button>
             </Link>
           </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 py-8">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 py-8">
             {products.slice(0, 5).map((item, index) => (
               <Product key={index} product={item} />
             ))}
