8276a508ed5cebaebed10f35040f4ecd88589b5a
Ashakuur1 <engshakrayare114@gmail.com>
2025-03-25 16:50:56 +0300

    Add CtaSection and ProductCategories to HomePage for improved layout and content organization

--- ui/src/pages/HomePage.tsx
+++ ui/src/pages/HomePage.tsx
@@ -1,7 +1,9 @@
 import AboutSection from "@/components/about-section";
 import CategoriesSection from "@/components/Categories/CategoriesSection";
 import CTA from "@/components/CTA";
+import CtaSection from "@/components/cta-2";
 import HeroSection from "@/components/HeroSection";
+import ProductCategories from "@/components/Products/ProductByCategory";
 import Products from "@/components/Products/products-section";
 import TestimonialsSection from "@/components/Testimonials";
 
@@ -12,8 +14,11 @@ const HomePage = () => {
       <AboutSection />
       <CategoriesSection />
       <Products />
-      <TestimonialsSection/>
+      <TestimonialsSection />
+      <CtaSection />
       <CTA />
+      <ProductCategories />
+
       {/* <CategorySection />
       <Products /> */}
       {/* <Footer /> */}
