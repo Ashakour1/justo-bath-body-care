12f45ef4d97357aaa9e3e5584e6f71da02d7c408
Ashakuur1 <engshakrayare114@gmail.com>
2025-05-03 14:49:00 +0300

    feat: add SEO metadata and Open Graph tags to HomePage component

--- ui/src/pages/HomePage.tsx
+++ ui/src/pages/HomePage.tsx
@@ -7,6 +7,7 @@ import ProductCategories from "@/components/Products/ProductByCategory";
 import Products from "@/components/Products/products-section";
 import TestimonialsSection from "@/components/Testimonials";
 import { useEffect } from "react";
+import { Helmet } from "react-helmet";
 
 const HomePage = () => {
   useEffect(() => {
@@ -15,6 +16,32 @@ const HomePage = () => {
 
   return (
     <main className="w-full h-full">
+      <Helmet>
+        <title>Justo Cosmetics</title>
+        <meta
+          name="description"
+          content="Welcome to Justo Cosmetics your go-to skincare brand for natural, effective, and affordable skin care products. Specializing in acne treatment, anti-aging creams, moisturizers, and face cleansers, Justo Cosmetics offers premium skincare solutions for all skin types."
+        />
+
+        <meta
+          name="keywords"
+          content="Justo, skincare, natural beauty, cosmetics, lotion, body cream, face cream, acne treatment, anti-aging, moisturizer, face cleanser, skin care products, beauty products, skincare routine, natural ingredients, cruelty-free, vegan skincare, affordable skincare products, premium skincare, skin types, skincare solutions, Justo Cosmetics, Justo Bath and Body Works, Justo Perfumes, Justo Rituals JUUSTO ABDI"
+        />
+        <link rel="canonical" href="https://www.justocosmetic.com/" />
+
+        {/* Open Graph  */}
+        <meta property="og:title" content="Justo Cosmetics" />
+        <meta
+          property="og:description"
+          content="Welcome to Justo Cosmetics your go-to skincare brand for natural, effective, and affordable skin care products. Specializing in acne treatment, anti-aging creams, moisturizers, and face cleansers, Justo Cosmetics offers premium skincare solutions for all skin types."
+        />
+        <meta property="og:image" content="/image.jpg" />
+        <meta property="og:url" content="https://justocosmetics.com" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Justo Cosmetics" />
+
+        {/* Twitter Card */}
+      </Helmet>
       <HeroSection />
       <AboutSection />
       <CategoriesSection />
