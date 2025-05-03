b98b8d65a3edef3bce1fab098ce773953741da2c
Ashakuur1 <engshakrayare114@gmail.com>
2025-05-03 12:04:15 +0300

    feat: add Helmet component for SEO optimization and social media sharing

--- ui/src/pages/HomePage.tsx
+++ ui/src/pages/HomePage.tsx
@@ -7,6 +7,7 @@ import ProductCategories from "@/components/Products/ProductByCategory";
 import Products from "@/components/Products/products-section";
 import TestimonialsSection from "@/components/Testimonials";
 import { useEffect } from "react";
+import { Helmet } from "react-helmet";
 
 const HomePage = () => {
   useEffect(() => {
@@ -15,6 +16,26 @@ const HomePage = () => {
 
   return (
     <div className="w-full h-full">
+      <Helmet>
+        <title>Justo Cosmetics</title>
+        <meta
+          name="description"
+          content="Welcome to Justo Cosmetics your go-to skincare brand for natural, effective, and affordable skin care products. Specializing in acne treatment, anti-aging creams, moisturizers, and face cleansers, Justo Cosmetics offers premium skincare solutions for all skin types."
+        />
+
+        {/* Open Graph  */}
+        <meta property="og:title" content="Justo Cosmetics" />
+        <meta
+          property="og:description"
+          content="Welcome to Justo Cosmetics your go-to skincare brand for natural, effective, and affordable skin care products. Specializing in acne treatment, anti-aging creams, moisturizers, and face cleansers, Justo Cosmetics offers premium skincare solutions for all skin types."
+        />
+        <meta property="og:image" content="/image.jpg" />
+        <meta property="og:url" content="https://justocosmetics.com" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Justo Cosmetics" />
+
+        {/* Twitter Card */}
+      </Helmet>
       <HeroSection />
       <AboutSection />
       <CategoriesSection />
