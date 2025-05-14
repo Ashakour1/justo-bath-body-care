fc5e4c1704d97503ab282d76ed715832cc0d52ee
Ashakuur1 <engshakrayare114@gmail.com>
2025-05-14 11:02:44 +0300

    fix: refactor routing to include ProtectedRoutes for dashboard access

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -9,28 +9,36 @@ import { ProductTable } from "./pages/Dashboard/ProductsPage";
 import { OrderTable } from "./pages/Dashboard/orders/ordersTable";
 import { CustomerTable } from "./pages/Dashboard/customers/customerTable";
 import OrderForm from "./pages/Dashboard/orders/EditOrder";
+import ProtectedRoutes from "./auth/protectedRoutes";
 
 function App() {
+  const auth = localStorage.getItem("auth");
+  const isAuthenticated = auth ? JSON.parse(auth).isAuthenticated : null;
+
   return (
     <>
       <Router>
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route
-            path="/dashboard"
-            element={
-              <>
-                <Sidebar />
-              </>
-            }
+            element={<ProtectedRoutes isAuthenticated={isAuthenticated} />}
           >
-            <Route index element={<DashboardPage />} />
-            <Route path="products" element={<ProductTable />} />
-            <Route path="products/add-product" element={<ProductForm />} />
-            <Route path="products/update/:id" element={<ProductForm />} />
-            <Route path="orders" element={<OrderTable />} />
-            <Route path="customers" element={<CustomerTable />} />
-            <Route path="orders/update/:id" element={<OrderForm />} />
+            <Route
+              path="/dashboard"
+              element={
+                <>
+                  <Sidebar />
+                </>
+              }
+            >
+              <Route index element={<DashboardPage />} />
+              <Route path="products" element={<ProductTable />} />
+              <Route path="products/add-product" element={<ProductForm />} />
+              <Route path="products/update/:id" element={<ProductForm />} />
+              <Route path="orders" element={<OrderTable />} />
+              <Route path="customers" element={<CustomerTable />} />
+              <Route path="orders/update/:id" element={<OrderForm />} />
+            </Route>
           </Route>
         </Routes>
       </Router>
