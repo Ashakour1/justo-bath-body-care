4e706d52d5f63d06b1475370c99da344a00be63c
Ashakuur1 <engshakrayare114@gmail.com>
2025-05-14 11:03:14 +0300

    feat: implement authentication check in LoginPage to redirect authenticated users

--- admin/src/pages/LoginPage.tsx
+++ admin/src/pages/LoginPage.tsx
@@ -1,6 +1,18 @@
+import { useNavigate } from "react-router-dom";
 import LoginForm from "../components/Login-form";
+import { useAuthStore } from "../store/store";
+import { useEffect } from "react";
 
 const LoginPage = () => {
+  const { isAuthenticated } = useAuthStore();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate("/dashboard");
+    }
+  }, [isAuthenticated, navigate]);
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
       <div className="w-full max-w-sm">
